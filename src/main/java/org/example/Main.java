777b7563aced9fba597764900fd66bc988345ec7
AKSHAT <akshatbanka477@gmail.com>
2024-03-08 21:19:00 +0530

    aa

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import views.Welcome;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Welcome w = new Welcome();
+        do{
+            w.welcomeScreen();
+        } while (true); 
+    }
+}
